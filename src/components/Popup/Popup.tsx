a113fcd9374c130b92fa658704ad6913865f3b7a
Deepak <upadhyaydeepak698@gmail.com>
2022-07-07 11:11:20 +0530

    form popup updated

--- src/components/Popup/Popup.tsx
+++ src/components/Popup/Popup.tsx
@@ -11,6 +11,8 @@ import {
   TVaccineName,
 } from 'types/patient'
 
+type TPopup = 'add' | 'edit' | 'view'
+
 interface IPopup {
   isLoading?: boolean
   head: string
@@ -22,6 +24,7 @@ interface IPopup {
   vaccinationStatusOptions: Array<TVaccinationStatus> | []
   vaccineNameOptions: Array<TVaccineName> | []
   symptomsOptions: Array<TSymptoms> | []
+  popUpType: TPopup
 }
 
 const Popup: FC<IPopup> = ({
@@ -253,7 +256,7 @@ const Popup: FC<IPopup> = ({
 
                 <div className='w-full'>
                   <div className='flex'>
-                    <div className='w-1/2 px-1 column'>
+                    <div className='w-full px-1 column'>
                       <label
                         htmlFor='symptoms'
                         className='block px-1 mb-2 text-sm font-medium text-gray-900 dark:text-gray-300'
@@ -263,7 +266,7 @@ const Popup: FC<IPopup> = ({
                       <select
                         name='symptoms'
                         id='symptoms'
-                        className='bg-gray-50 border border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                        className='bg-gray-50 border border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 h-36 overflow-auto'
                         multiple={true}
                       >
                         <option selected>Choose an option</option>
@@ -291,8 +294,8 @@ const Popup: FC<IPopup> = ({
                       <textarea
                         id='anyMedicalHistory'
                         name='anyMedicalHistory'
-                        aria-rowspan={4}
-                        className='block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 resize-none'
+                        aria-rowspan={8}
+                        className='block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 resize-none h-36'
                         placeholder='Your message...'
                       ></textarea>
                     </div>
